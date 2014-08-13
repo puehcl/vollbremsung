71e710a078b0744b48a99aa5b53e511676fb0c91
Maximilian Irro <max@disposia.org>
2014-08-13 11:17:45 +0200

    now a true "compression ratio" is outputted; handbrake error handling now with rescue - not CLI exit code

--- bin/vollbremsung.rb
+++ bin/vollbremsung.rb
@@ -73,16 +73,10 @@ if __FILE__ == $0
     exit 1
   end
   
-  unless find_executable('HandbrakeCLI') 
-    unless find_executable('HandBrakeCLI')
-      puts "It seems you do not have HandbrakeCLI installed or it is not available in your $PATH."
-      puts "Install it an run again"
-      exit 1
-    else
-      HANDBRAKE_CLI = "HandBrakeCLI" # this is the FreeBSD version
-    end
-  else
-    HANDBRAKE_CLI = "HandbrakeCLI" # OSX version
+  unless find_executable('HandbrakeCLI') || find_executable('HandBrakeCLI')
+    puts "It seems you do not have HandbrakeCLI installed or it is not available in your $PATH."
+    puts "Install it an run again"
+    exit 1
   end
   
   unless find_executable 'ffprobe'
@@ -146,24 +140,22 @@ if __FILE__ == $0
     log "processing: #{infile}"
 
     #%x( #{HANDBRAKE_CLI} #{HANDBRAKE_OPTIONS} --audio #{(1..astreams.count).to_a.join(',')} --aname #{astreams.names.join(',')} --subtitle #{(1..sstreams.count).to_a.join(',')} -i \"#{infile}\" -o \"#{outfile}\" 2>&1 )
-
-
-    HandBrake::CLI.new.input(infile).encoder('x264').quality('20.0').aencoder('faac').
-    ab('160').mixdown('dpl2').arate('Auto').drc('0.0').format('mp4').markers.
-    audio_copy_mask('aac,ac3,dtshd,dts,mp3').audio_fallback('ffac3').x264_preset('veryfast').
-    loose_anamorphic.modulus('2').audio((1..astreams.count).to_a.join(',')).aname(astreams.names.join(',')).
-    subtitle((1..sstreams.count).to_a.join(',')).output(outfile)
-
     
-    
-    if $?.exitstatus == 0
+    begin
+      
+      HandBrake::CLI.new.input(infile).encoder('x264').quality('20.0').aencoder('faac').
+      ab('160').mixdown('dpl2').arate('Auto').drc('0.0').format('mp4').markers.
+      audio_copy_mask('aac,ac3,dtshd,dts,mp3').audio_fallback('ffac3').x264_preset('veryfast').
+      loose_anamorphic.modulus('2').audio((1..astreams.count).to_a.join(',')).aname(astreams.names.join(',')).
+      subtitle((1..sstreams.count).to_a.join(',')).output(outfile)
+      
+      # if we make it here, encoding went well
       log "SUCCESS: encoding done"
       
-      insize = File.size(infile)
-      outsize = File.size(outfile)
-      log "insize: #{insize}, outsize: #{outsize}"
-      
-      log "Output file is #{(outsize - insize) / insize * 100} % smaller"
+      infile_size = File.size(infile)
+      outfile_size = File.size(outfile)
+
+      log "Compression ratio: %.2f" % (outfile_size.to_f / infile_size.to_f)
       
       if options[:rename]
         log "renaming #{infile} to .old"
@@ -176,22 +168,23 @@ if __FILE__ == $0
         tmpfile = filename + ".tmp.mp4"
         
         %x( ffmpeg -i \"#{outfile}\" -metadata title=\"#{filename}\" #{FFMPEG_OPTIONS} \"#{tmpfile}\" 2>&1 )
-         if $?.exitstatus == 0
-           begin
-             File.delete outfile
-             File.rename tmpfile, outfile 
-           rescue
-             log "ERROR: moving #{tmpfile} to #{outfile}"
-           end
-         else
-           log "ERROR: mp4 title could not be changed"
-         end
+        
+        if $?.exitstatus == 0
+          begin
+            File.delete outfile
+            File.rename tmpfile, outfile 
+          rescue
+            log "ERROR: moving #{tmpfile} to #{outfile}"
+          end
+        else
+          log "ERROR: mp4 title could not be changed"
+        end
       end
-    
-    else 
+    rescue 
       log "ERROR: Handbrake exited with an error"
     end
-  end
+    
+  end # target_files.each
   
   if target_files.empty?
     log "nothing to do"
