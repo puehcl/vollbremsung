43a963d1c501b3f8141a27ea97a92027fc1e7c64
Maximilian Irro <max@disposia.org>
2014-08-13 13:45:35 +0200

    added recursive feature; it works, but it does all with absolute paths, which makes it a bit hard to get all those filename parts

--- bin/vollbremsung.rb
+++ bin/vollbremsung.rb
@@ -36,8 +36,13 @@ if __FILE__ == $0
       end
       opts.separator ""
     
-      opts.on("-r", "--rename", "Rename source files to <FILENAME>.old after successful encoding") do |flag|
-        options[:rename] = true
+      opts.on("-m", "--move", "Move source files to <FILENAME>.old after successful encoding") do |flag|
+        options[:move] = true
+      end
+      opts.separator ""
+      
+      opts.on("-r", "--recursive", "Process all file in subdirectories recursively as well") do |flag|
+        options[:recursive] = true
       end
       opts.separator ""
       
@@ -56,7 +61,9 @@ if __FILE__ == $0
     puts "Option parsing not supported on your system. All options will be ignored."
     puts "To enable support run 'gem install optparse'"
     options[:delete] = false
-    options[:rename] = false
+    options[:move] = false
+    options[:recursive] = false
+    options[:title] = false
   end
 
 
@@ -96,19 +103,29 @@ if __FILE__ == $0
   log "probing for target files..."
   target_files = []
   if File.directory?(TARGET_PATH)
-    Dir.entries(TARGET_PATH).each do |file|
-      target_files << file if CONVERT_TYPES.include?(File.extname(file).downcase[1..-1])
+    
+    scope = options[:recursive] ? "/**/*" : "/*"
+    
+    Dir[TARGET_PATH + scope].each do |file|
+      unless File.directory?(file)
+        target_files << file if CONVERT_TYPES.include?(File.extname(file).downcase[1..-1]) 
+      end
     end
+
+    #Dir.entries(TARGET_PATH).each do |file|
+    #  target_files << file if CONVERT_TYPES.include?(File.extname(file).downcase[1..-1])
+    #end
   else
-    target_files << TARGET_PATH
+    target_files << File.absolute_path(TARGET_PATH)
   end
   
   log "Files found:"
   target_files.each do |file|
-    puts "* #{file}"
+    relative_path = file[TARGET_PATH.length+1..-1]
+    puts "* #{relative_path}"
   end
 
-  Dir.chdir(TARGET_PATH)
+  #Dir.chdir(TARGET_PATH)
   
   target_files.each do |infile|
     
@@ -134,10 +151,18 @@ if __FILE__ == $0
       end
     end
 
-    filename = File.basename(infile, File.extname(infile)) # without ext
-    outfile = "#{filename}.mp4"
+    infile_noext = File.join( File.dirname(infile), File.basename(infile,File.extname(infile)))
+    infile_basename = File.basename(infile)
+    infile_basename_noext = File.basename(infile, File.extname(infile)) # without ext
+    infile_dirname = File.dirname(infile)
+    infile_relative = infile[infile_dirname.length+1..-1]
+
+    #filename = File.basename(infile, File.extname(infile)) # without ext
+    #outfile = "#{filename}.mp4"
+    outfile = "#{File.join(infile_dirname, infile_basename_noext)}.mp4"
     
-    log "processing: #{infile}"
+    #log "processing: #{infile}"
+    log "processing: #{infile_relative}" 
 
     #%x( #{HANDBRAKE_CLI} #{HANDBRAKE_OPTIONS} --audio #{(1..astreams.count).to_a.join(',')} --aname #{astreams.names.join(',')} --subtitle #{(1..sstreams.count).to_a.join(',')} -i \"#{infile}\" -o \"#{outfile}\" 2>&1 )
     
@@ -157,15 +182,19 @@ if __FILE__ == $0
 
       log "Compression ratio: %.2f" % (outfile_size.to_f / infile_size.to_f)
       
-      if options[:rename]
-        log "renaming #{infile} to .old"
-        File.rename infile, "#{infile}.old" 
+      if options[:move]
+        #log "renaming #{infile} to .old"
+        #File.rename infile, "#{infile}.old" 
+        
+        log "moveing #{infile_relative} to *.old"
+        File.rename infile, "#{infile}.old"
       end
       
       if options[:title]
         log "setting mp4 title"
         
-        tmpfile = filename + ".tmp.mp4"
+        #tmpfile = filename + ".tmp.mp4"
+        tmpfile = infile_noext + ".tmp.mp4"
         
         %x( ffmpeg -i \"#{outfile}\" -metadata title=\"#{filename}\" #{FFMPEG_OPTIONS} \"#{tmpfile}\" 2>&1 )
         
