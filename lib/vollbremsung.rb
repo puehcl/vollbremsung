8879e9a344a065d38f81875ab56db82a7ca14d1c
Maximilian Irro <max@disposia.org>
2014-08-14 11:49:03 +0200

    added option to change the used x264-preset

--- lib/vollbremsung.rb
+++ lib/vollbremsung.rb
@@ -4,6 +4,7 @@ module Vollbremsung
   VERSION = '0.0.8'
   CONVERT_TYPES = ['mkv','avi','mov','flv','mpg','wmv','ogm']
   FFMPEG_OPTIONS = "-map 0 -acodec copy -vcodec copy -scodec copy"
+  X264_DEFAULT_PRESET = "veryfast"
   
   class StreamDescr < Struct.new(:count,:names)
     def initialize
