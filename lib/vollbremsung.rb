3360a156ff9bf56bc43f94abfd2c2d88e78d48da
Maximilian Irro <max@disposia.org>
2014-09-14 10:29:43 +0200

    v0.0.17

--- lib/vollbremsung.rb
+++ lib/vollbremsung.rb
@@ -1,7 +1,7 @@
 module Vollbremsung
   
   USAGE   = "Usage: vollbremsung [options] target [target [...]]"
-  VERSION = '0.0.16'
+  VERSION = '0.0.17'
   CONVERT_TYPES = ['mkv','avi','mov','flv','mpg','wmv','ogm']
   FFMPEG_OPTIONS = "-map 0 -acodec copy -vcodec copy -scodec copy"
   X264_DEFAULT_PRESET = "veryfast"
